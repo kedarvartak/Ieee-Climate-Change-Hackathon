8f09fae424a82ca15ad62f7b1cf7141bed45e8ef
Kedar Vartak <kedar.vartak22@vit.edu>
2024-10-14 18:36:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/SuPZ5YJddQQYLm6n/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/kcbwZyWFJq8RVVEU/scene.splinecode" />
   );
 }
